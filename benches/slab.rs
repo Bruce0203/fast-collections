2a2005eef1c93cc501294cbe62cd404212d1b83a
Bruce0203 <BruceFreedy@gmail.com>
2024-06-27 20:50:27 +0900

    add benchmark

---
+++ benches/slab.rs
@@ -0,0 +1,32 @@
+use std::{hint::black_box, rc::Weak};
+
+use criterion::{Criterion, Throughput};
+use fast_collections::{Cursor, CursorReadTransmute, GetUnchecked, Slab};
+use rand::Rng;
+
+fn bench(c: &mut Criterion) {
+    let mut slab: Slab<MyStruct, typenum::U500> = Slab::new();
+    let value: u8 = rand::thread_rng().gen();
+    let value = value as usize;
+    let mut group = c.benchmark_group("pointer");
+    group.throughput(Throughput::Bytes(1000));
+    {
+        let value = unsafe { slab.get_unchecked_mut(value) };
+        group.bench_function("get with static pointer", |b| {
+            b.iter(|| {
+                black_box(value.0);
+            });
+        });
+    }
+    group.bench_function("get with relative pointer", |b| {
+        b.iter(|| {
+            let value = unsafe { slab.get_unchecked_mut(value) };
+            black_box(value.0);
+        });
+    });
+}
+
+pub struct MyStruct(usize);
+
+criterion::criterion_group!(benches, bench);
+criterion::criterion_main!(benches);
