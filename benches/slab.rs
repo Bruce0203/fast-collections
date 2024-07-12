ecf085be16cf37cf4219dd615a4ef3ac537abc8c
Bruce0203 <BruceFreedy@gmail.com>
2024-07-12 22:37:56 +0900

    update benchmark

--- benches/slab.rs
+++ benches/slab.rs
@@ -5,30 +5,27 @@ use fast_collections::{Cursor, CursorReadTransmute, GetUnchecked, Slab};
 use rand::Rng;
 
 fn bench(c: &mut Criterion) {
-    let mut slab: Slab<MyStruct, typenum::U500> = Slab::new();
     let mut group = c.benchmark_group("pointer");
     group.throughput(Throughput::Bytes(1000));
     {
-        let value = rand::thread_rng().gen::<u8>() as usize;
+        let mut slice: [u8; 100] = [rand::thread_rng().gen(); 100];
+        let index: usize = rand::thread_rng().gen::<u8>() as usize;
         group.bench_function("get with relative pointer", |b| {
             b.iter(|| {
-                let value = unsafe { slab.get_unchecked_mut(value) };
-                black_box(value.0);
+                black_box(*unsafe { slice.get_unchecked_mut(index) });
             });
         });
     }
     {
-        let value = rand::thread_rng().gen::<u8>() as usize;
-        let value = unsafe { slab.get_unchecked_mut(value) };
+        let mut value = rand::thread_rng().gen::<u8>();
+        let value: &mut u8 = black_box(&mut value);
         group.bench_function("get with static pointer", |b| {
             b.iter(|| {
-                black_box(value.0);
+                black_box(*value);
             });
         });
     }
 }
-
-pub struct MyStruct(usize);
 
 criterion::criterion_group!(benches, bench);
 criterion::criterion_main!(benches);
